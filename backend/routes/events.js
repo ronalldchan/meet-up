a2ab76881bc8961c2872a3bde2b173b596d44501
Ronald Chan <ronaldc.rcws@gmail.com>
2024-08-10 13:22:26 -0700

    Updated events route

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Sending list of all events");
-});
+events = [];
+
+class Event {
+    id;
+    name;
+    users;
+
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.users = new Map();
+    }
+}
 
 router
-    .route("/:id")
-    .post((req, res) => {
-        res.send("Creating new event");
-    })
+    .route("/")
     .get((req, res) => {
-        res.send(`Get event with ID ${req.params.id}`);
+        res.status(200).send(events);
+    })
+    .post((req, res) => {
+        const name = req.body.name;
+        if (!name || name.length < 3) {
+            res.status(400).send("Name is required and should be minimum of 3 characters");
+            return;
+        }
+        let event = new Event(events.length + 1, name);
+        events.push(event);
+        res.status(200).send(event);
     });
+
+router.route("/:id").get((req, res) => {
+    res.send(`Get event with ID ${req.params.id}`);
+});
 
 router.route("/:id/:user").post((req, res) => {
     res.send("Creating new user");
