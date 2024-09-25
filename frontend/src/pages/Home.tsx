d50613ed492ca9c27aa585bc088f293a88891c4b
Ronald Chan <ronaldc.rcws@gmail.com>
2024-09-24 18:09:24 -0700

    added tzdb and date-fns-tz library added state values for today, next year, earliest time, latest time, and timezone added timezone form and create event button

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,17 @@
-import { Box, Button, Container, Grid2, Paper, TextField, Typography } from "@mui/material";
+import {
+    Box,
+    Button,
+    Container,
+    FormControl,
+    Grid2,
+    InputLabel,
+    MenuItem,
+    Paper,
+    Select,
+    Stack,
+    TextField,
+    Typography,
+} from "@mui/material";
 import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import React from "react";
@@ -6,31 +19,36 @@ import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 import "../DayPicker.css";
 import { isBefore, isSameMinute, set } from "date-fns";
+import { getTimeZones } from "@vvo/tzdb";
+import { fromZonedTime, toZonedTime } from "date-fns-tz";
 
 function Home() {
     const [selected, setSelected] = React.useState<Date[]>([]);
-    // const som = Intl.supportedValuesOf("timeZone");
-    const today: Date = new Date();
-    const [earliest, setEarliest] = React.useState<Date>(set(today, { hours: 9, minutes: 0 }));
-    const [latest, setLatest] = React.useState<Date>(set(today, { hours: 17, minutes: 0 }));
+    const timezones = getTimeZones()
+        .map((value) => value.name)
+        .sort();
+    const [today, setToday] = React.useState<Date>(new Date());
+    const [nextYear, setNextYear] = React.useState<Date>(set(today, { year: today.getFullYear() + 1 }));
+
+    const [earliestTime, setEarliestTime] = React.useState<Date>(set(today, { hours: 9, minutes: 0 }));
+    const [latestTime, setLatestTime] = React.useState<Date>(set(today, { hours: 17, minutes: 0 }));
+    const [timezone, setTimezone] = React.useState<string>(Intl.DateTimeFormat().resolvedOptions().timeZone);
     return (
         <Container>
             <Typography variant="h1" align="center">
                 Meet Up
             </Typography>
-
             <Grid2 container spacing={5} justifyContent={"center"}>
                 <Grid2>
                     <Paper sx={{ padding: 3 }}>
                         <DayPicker
                             mode="multiple"
-                            // captionLayout="dropdown"
                             startMonth={today}
-                            endMonth={new Date(today.getFullYear() + 1, today.getMonth())}
+                            endMonth={nextYear}
+                            today={today}
                             selected={selected}
                             onSelect={setSelected}
                             disabled={{ before: today }}
-                            // showOutsideDays
                             required
                             footer={
                                 <Box
@@ -56,46 +74,67 @@ function Home() {
                 </Grid2>
                 <Grid2 display={"flex"} justifyContent={"center"} alignItems={"center"}>
                     <Box>
-                        <Box padding={3}>
-                            <TextField label="Event Name" required />
-                        </Box>
-                        {/* <Typography>test</Typography> */}
-                        <LocalizationProvider dateAdapter={AdapterDateFns}>
-                            <TimePicker
-                                minutesStep={15}
-                                label="Earliest Start Time"
-                                value={earliest}
-                                onChange={(date) => (date !== null ? setEarliest(date) : null)}
-                                skipDisabled={true}
-                                slotProps={{
-                                    field: {
-                                        readOnly: true,
-                                    },
-                                }}
-                            />
-                            <TimePicker
-                                minutesStep={15}
-                                label="Latest End Time"
-                                value={latest}
-                                onChange={(date) => (date !== null ? setLatest(date) : null)}
-                                skipDisabled={true}
-                                slotProps={{
-                                    field: {
-                                        readOnly: true,
-                                    },
-                                }}
-                                shouldDisableTime={(value) =>
-                                    isBefore(value, earliest) || isSameMinute(value, earliest)
-                                }
-                            />
-                        </LocalizationProvider>
+                        <Stack spacing={2}>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <TimePicker
+                                    minutesStep={15}
+                                    label="Earliest Start Time"
+                                    value={earliestTime}
+                                    onChange={(date) => (date !== null ? setEarliestTime(date) : null)}
+                                    skipDisabled={true}
+                                    slotProps={{
+                                        field: {
+                                            readOnly: true,
+                                        },
+                                    }}
+                                />
+                                <TimePicker
+                                    minutesStep={15}
+                                    label="Latest End Time"
+                                    value={latestTime}
+                                    onChange={(date) => (date !== null ? setLatestTime(date) : null)}
+                                    skipDisabled={true}
+                                    slotProps={{
+                                        field: {
+                                            readOnly: true,
+                                        },
+                                    }}
+                                    shouldDisableTime={(value) =>
+                                        isBefore(value, earliestTime) || isSameMinute(value, earliestTime)
+                                    }
+                                />
+                            </LocalizationProvider>
+                            <FormControl fullWidth>
+                                <InputLabel id="timezone">Timezone</InputLabel>
+                                <Select
+                                    labelId="timezone"
+                                    label="Timezone"
+                                    value={timezone}
+                                    onChange={(event) => {
+                                        const value: string = event.target.value as string;
+                                        const utcTime: Date = fromZonedTime(today, timezone);
+                                        const zonedTime = toZonedTime(utcTime, value);
+                                        setToday(zonedTime);
+                                        setNextYear(set(zonedTime, { year: today.getFullYear() + 1 }));
+                                        setTimezone(value);
+                                    }}
+                                >
+                                    {timezones.map((value) => (
+                                        <MenuItem value={value}>{value}</MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <Box justifyContent={"center"} display={"flex"}>
+                                <TextField label="Event Name" fullWidth />
+                            </Box>
+                            <Button variant="contained">Create Event</Button>
+                        </Stack>
                     </Box>
                 </Grid2>
             </Grid2>
-
             <Typography> {String(selected)} </Typography>
-            <Typography> {String(earliest)} </Typography>
-            <Typography> {String(latest)} </Typography>
+            <Typography> {String(earliestTime)} </Typography>
+            <Typography> {String(latestTime)} </Typography>
         </Container>
     );
 }
