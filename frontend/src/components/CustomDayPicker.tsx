af4f0997bf9292256bf148002472fc21fbd0576d
Ronald Chan <ronaldc.rcws@gmail.com>
2024-09-24 21:38:04 -0700

    Created CustomDayPicker component to easily read Home.tsx Now disable create event button if conditions are not met

---
+++ frontend/src/components/CustomDayPicker.tsx
@@ -0,0 +1,47 @@
+import { Box, Button, Paper, Typography } from "@mui/material";
+import React from "react";
+import { DayPicker } from "react-day-picker";
+
+interface CustomDayPickerProps {
+    dates: Date[];
+    setSelected: (dates: Date[]) => void;
+    today: Date;
+    nextYear: Date;
+}
+
+export function CustomDayPicker({ dates, setSelected, today, nextYear }: CustomDayPickerProps) {
+    return (
+        <Paper sx={{ padding: 3 }}>
+            <DayPicker
+                mode="multiple"
+                startMonth={today}
+                endMonth={nextYear}
+                today={today}
+                selected={dates}
+                onSelect={setSelected}
+                disabled={{ before: today }}
+                showOutsideDays
+                required
+                footer={
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                        }}
+                    >
+                        {dates.length > 0 ? (
+                            <Typography>You selected {dates?.length} days.</Typography>
+                        ) : (
+                            <Typography>Please select at least one day.</Typography>
+                        )}
+
+                        <Button sx={{ marginLeft: "auto" }} onClick={() => setSelected([])}>
+                            Reset
+                        </Button>
+                    </Box>
+                }
+            />
+        </Paper>
+    );
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,7 +6,6 @@ import {
     Grid2,
     InputLabel,
     MenuItem,
-    Paper,
     Select,
     Stack,
     TextField,
@@ -15,24 +14,24 @@ import {
 import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import React from "react";
-import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 import "../DayPicker.css";
 import { isBefore, isSameMinute, set } from "date-fns";
-import { getTimeZones } from "@vvo/tzdb";
+import { rawTimeZones } from "@vvo/tzdb";
 import { fromZonedTime, toZonedTime } from "date-fns-tz";
+import { CustomDayPicker } from "../components/CustomDayPicker";
 
 function Home() {
-    const [selected, setSelected] = React.useState<Date[]>([]);
-    const timezones = getTimeZones()
-        .map((value) => value.name)
-        .sort();
     const [today, setToday] = React.useState<Date>(new Date());
     const [nextYear, setNextYear] = React.useState<Date>(set(today, { year: today.getFullYear() + 1 }));
+
+    const timezones = rawTimeZones.map((value) => value.name).sort();
+    const [dates, setSelected] = React.useState<Date[]>([]);
 
     const [earliestTime, setEarliestTime] = React.useState<Date>(set(today, { hours: 9, minutes: 0 }));
     const [latestTime, setLatestTime] = React.useState<Date>(set(today, { hours: 17, minutes: 0 }));
     const [timezone, setTimezone] = React.useState<string>(Intl.DateTimeFormat().resolvedOptions().timeZone);
+    const [eventName, setEventName] = React.useState<string>("");
     return (
         <Container>
             <Typography variant="h1" align="center">
@@ -40,37 +39,7 @@ function Home() {
             </Typography>
             <Grid2 container spacing={5} justifyContent={"center"}>
                 <Grid2>
-                    <Paper sx={{ padding: 3 }}>
-                        <DayPicker
-                            mode="multiple"
-                            startMonth={today}
-                            endMonth={nextYear}
-                            today={today}
-                            selected={selected}
-                            onSelect={setSelected}
-                            disabled={{ before: today }}
-                            required
-                            footer={
-                                <Box
-                                    sx={{
-                                        display: "flex",
-                                        justifyContent: "space-between",
-                                        alignItems: "center",
-                                    }}
-                                >
-                                    {selected.length > 0 ? (
-                                        <Typography>You selected {selected?.length} days.</Typography>
-                                    ) : (
-                                        <Typography>Please select at least one day.</Typography>
-                                    )}
-
-                                    <Button sx={{ marginLeft: "auto" }} onClick={() => setSelected([])}>
-                                        Reset
-                                    </Button>
-                                </Box>
-                            }
-                        />
-                    </Paper>
+                    <CustomDayPicker dates={dates} setSelected={setSelected} today={today} nextYear={nextYear} />
                 </Grid2>
                 <Grid2 display={"flex"} justifyContent={"center"} alignItems={"center"}>
                     <Box>
@@ -120,19 +89,34 @@ function Home() {
                                     }}
                                 >
                                     {timezones.map((value) => (
-                                        <MenuItem value={value}>{value}</MenuItem>
+                                        <MenuItem key={value} value={value}>
+                                            {value}
+                                        </MenuItem>
                                     ))}
                                 </Select>
                             </FormControl>
-                            <Box justifyContent={"center"} display={"flex"}>
-                                <TextField label="Event Name" fullWidth />
-                            </Box>
-                            <Button variant="contained">Create Event</Button>
+                            <TextField
+                                label="Event Name"
+                                fullWidth
+                                value={eventName}
+                                onChange={(event) => setEventName(event.target.value)}
+                                error={eventName.length < 3}
+                                placeholder="My New Event"
+                                color={eventName.length != 0 ? "success" : "primary"}
+                            />
+                            <Button
+                                variant="contained"
+                                disabled={
+                                    !(dates.length > 0 && isBefore(earliestTime, latestTime) && eventName.length >= 3)
+                                }
+                            >
+                                Create Event
+                            </Button>
                         </Stack>
                     </Box>
                 </Grid2>
             </Grid2>
-            <Typography> {String(selected)} </Typography>
+            <Typography> {String(dates)} </Typography>
             <Typography> {String(earliestTime)} </Typography>
             <Typography> {String(latestTime)} </Typography>
         </Container>
