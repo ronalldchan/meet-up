597d375954957edbe40bb2f5509fc9a4f8c8b800
Ronald Chan <ronaldc.rcws@gmail.com>
2025-03-10 18:11:40 -0700

    Removed excess components and effects. removed progress loading animation in favor of alert notification

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, CircularProgress, Container, Grid2, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, Grid2, Stack, TextField, Typography } from "@mui/material";
 import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import React from "react";
@@ -78,43 +78,41 @@ function Home() {
             </Typography>
             <Grid2 container spacing={5} justifyContent={"center"}>
                 <Grid2>
-                    {/* <CustomDayPicker dates={dates} setSelected={setDates} today={today} nextYear={nextYear} /> */}
                     <CustomDayPicker dates={dates} setSelected={setDates} />
                 </Grid2>
                 <Grid2 display={"flex"} justifyContent={"center"} alignItems={"center"}>
                     <form onSubmit={createEvent}>
-                        <Box>
-                            <Stack spacing={2}>
-                                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                    <TimePicker
-                                        minutesStep={minuteStep}
-                                        label="Earliest Start Time"
-                                        value={earliestTime}
-                                        onChange={(date) => (date !== null ? setEarliestTime(date) : null)}
-                                        skipDisabled={true}
-                                        slotProps={{
-                                            field: {
-                                                readOnly: true,
-                                            },
-                                        }}
-                                    />
-                                    <TimePicker
-                                        minutesStep={minuteStep}
-                                        label="Latest End Time"
-                                        value={latestTime}
-                                        onChange={(date) => (date !== null ? setLatestTime(date) : null)}
-                                        skipDisabled={true}
-                                        slotProps={{
-                                            field: {
-                                                readOnly: true,
-                                            },
-                                        }}
-                                        // shouldDisableTime={(value) =>
-                                        //     isBefore(value, earliestTime) || isSameMinute(value, earliestTime)
-                                        // }
-                                    />
-                                </LocalizationProvider>
-                                {/* {
+                        <Stack spacing={2}>
+                            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                                <TimePicker
+                                    minutesStep={minuteStep}
+                                    label="Earliest Start Time"
+                                    value={earliestTime}
+                                    onChange={(date) => date && setEarliestTime(date)}
+                                    skipDisabled={true}
+                                    slotProps={{
+                                        field: {
+                                            readOnly: true,
+                                        },
+                                    }}
+                                />
+                                <TimePicker
+                                    minutesStep={minuteStep}
+                                    label="Latest End Time"
+                                    value={latestTime}
+                                    onChange={(date) => (date !== null ? setLatestTime(date) : null)}
+                                    skipDisabled={true}
+                                    slotProps={{
+                                        field: {
+                                            readOnly: true,
+                                        },
+                                    }}
+                                    // shouldDisableTime={(value) =>
+                                    //     isBefore(value, earliestTime) || isSameMinute(value, earliestTime)
+                                    // }
+                                />
+                            </LocalizationProvider>
+                            {/* {
                                     <FormControl fullWidth>
                                         <InputLabel id="timezone">Timezone</InputLabel>
                                         <Select
@@ -131,39 +129,24 @@ function Home() {
                                         </Select>
                                     </FormControl>
                                 } */}
-                                <TextField
-                                    label="Event Name"
-                                    fullWidth
-                                    value={eventName}
-                                    onChange={(event) => setEventName(event.target.value)}
-                                    error={eventName.length < 3}
-                                    placeholder="My New Event"
-                                    color={eventName.length != 0 ? "success" : "primary"}
-                                />
-                                <Box sx={{ position: "relative" }}>
-                                    <Button
-                                        variant="contained"
-                                        type="submit"
-                                        disabled={!enableSubmitButton || loading}
-                                        fullWidth
-                                    >
-                                        Create Event
-                                    </Button>
-                                    {loading && (
-                                        <CircularProgress
-                                            size={24}
-                                            sx={{
-                                                position: "absolute",
-                                                top: "50%",
-                                                left: "50%",
-                                                marginTop: "-12px",
-                                                marginLeft: "-12px",
-                                            }}
-                                        />
-                                    )}
-                                </Box>
-                            </Stack>
-                        </Box>
+                            <TextField
+                                label="Event Name"
+                                fullWidth
+                                value={eventName}
+                                onChange={(event) => setEventName(event.target.value)}
+                                error={eventName.length < 3}
+                                placeholder="My New Event"
+                            />
+
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                disabled={!enableSubmitButton || loading}
+                                fullWidth
+                            >
+                                Create Event
+                            </Button>
+                        </Stack>
                     </form>
                 </Grid2>
             </Grid2>
@@ -174,14 +157,16 @@ function Home() {
                 onClose={() => setError(null)}
                 severity="success"
             />
-            <Typography variant="h5" fontWeight={"bold"}>
-                Debugging
-            </Typography>
-            <Typography> Selected Dates: {String(dates)} </Typography>
-            <Typography> Today: {String(today)}</Typography>
-            {/* <Typography> Next Year: {String(nextYear)}</Typography> */}
-            <Typography> Earliest Time: {String(earliestTime)} </Typography>
-            <Typography> Latest Time: {String(latestTime)} </Typography>
+            <Box>
+                <Typography variant="h5" fontWeight={"bold"}>
+                    Debugging
+                </Typography>
+                <Typography> Selected Dates: {String(dates)} </Typography>
+                <Typography> Today: {String(today)}</Typography>
+                {/* <Typography> Next Year: {String(nextYear)}</Typography> */}
+                <Typography> Earliest Time: {String(earliestTime)} </Typography>
+                <Typography> Latest Time: {String(latestTime)} </Typography>
+            </Box>
         </Container>
     );
 }
